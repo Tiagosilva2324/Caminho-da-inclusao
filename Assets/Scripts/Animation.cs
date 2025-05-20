15a7f04c74b0408664c4246a42d067d3cc38c581
Dev Tiago Silva <162350803+Tiagosilva2324@users.noreply.github.com>
2025-05-20 20:12:37 -0300

    Animtion of player complete!!!!

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animator playerAnimator;
+    float input_x = 0;
+    float input_y = 0;
+    bool isWalking = false;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isWalking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input_x = Input.GetAxisRaw("Horizontal");
+        input_y = Input.GetAxisRaw("Vertical");
+        isWalking = (input_x != 0 || input_y != 0);
+
+        if (isWalking) { 
+        var move = new Vector3(input_x, input_y, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+            playerAnimator.SetFloat("input_x", input_x);
+            playerAnimator.SetFloat("input_y", input_y);
+        }
+        playerAnimator.SetBool("isWalking", isWalking);
+    }
+}
